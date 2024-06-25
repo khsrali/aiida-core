421e4e3f3925da328c8a144d6bc2265d58e91b46
Ali <khsrali@gmail.com>
2024-06-25 07:44:56 +0200

    added initializer

--- src/aiida/schedulers/plugins/__init__.py
+++ src/aiida/schedulers/plugins/__init__.py
@@ -6,3 +6,8 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
+from .bash import *
+
+__all__ = (
+    'BashCliScheduler',
+)
